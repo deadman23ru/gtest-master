b904c3a18bbdfd340152fd3c26dc2738f4f650cb
deadman23ru <maxErmolaev2002@mail.ru>
2021-03-23 22:48:31 +0300

    Initial commit

---
+++ app/func.c
@@ -0,0 +1,32 @@
+#include <math.h>
+#include "func.h"
+
+solution Func(double a, double b, double c) {
+    solution res;
+
+    if(a == 0){
+        if(b == 0){
+            res.count = 0;
+            return res;
+        }else{
+        res.x1 = (-c)/b;
+        return res;
+        }
+    }
+
+    double D = pow(b, 2) - 4*a*c;
+
+    if(D < 0.0) {
+        res.count = 0;
+
+    }
+    if(D == 0.0) {
+        res.x1 = -b/(2*a);
+    }
+    if(D > 0.0) {
+        res.x1 = (-b - sqrt(D)) / (2*a);
+        res.x2 = (-b + sqrt(D)) / (2*a);
+    }
+
+    return res;
+}

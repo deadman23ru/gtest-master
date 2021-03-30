ec5a43b6d094a49522ba9cd53657de3f7b19ef78
deadman23ru <78728807+deadman23ru@users.noreply.github.com>
2021-03-30 12:19:31 +0300

    Update func_test.h

--- tests/func_test.h
+++ tests/func_test.h
@@ -13,22 +13,26 @@ TEST(Func, no_sol) {
 TEST(Func, one_sol) {
     solution res = Func(4, 4, 1);
     EXPECT_EQ(-0.5, res.x1);
+    EXPECT_EQ(1, res.count);
 }
 
 TEST(Func, two_sol) {
     solution res = Func(2, 5, 2);
     EXPECT_EQ(-2, res.x1);
     EXPECT_EQ(-0.5, res.x2);
+    EXPECT_EQ(2, res.count);
 }
 TEST(Func, test4_negative){
     solution res = Func(0, 1, 1);
     EXPECT_EQ(-1, res.x1);
+    EXPECT_EQ(1, res.count);
 
 }
 TEST(Func, test5_negative){
     solution res = Func(1, 0, -1);
     EXPECT_EQ(-1, res.x1);
     EXPECT_EQ(1, res.x2);
+    EXPECT_EQ(2, res.count);
 
 }
 TEST(Func, test6_negative){
@@ -44,6 +48,8 @@ TEST(Func, test7_negative){
 TEST(Func, test8_negative){
     solution res = Func(0, 1, 0);
     EXPECT_EQ(0, res.x1);
+    EXPECT_EQ(1, res.count);
+    
 
 }
 TEST(Func, test9_negative){
@@ -55,11 +61,13 @@ TEST(Func, test10_negative){
     solution res = Func(1, 1, 0);
     EXPECT_EQ(-1, res.x1);
     EXPECT_EQ(0, res.x2);
+    EXPECT_EQ(1, res.count);
 
 }
 TEST(Func, test11_negative){
     solution res = Func(1, 0, 0);
     EXPECT_EQ(0, res.x1);
+    EXPECT_EQ(1, res.count);
 
 
 }
